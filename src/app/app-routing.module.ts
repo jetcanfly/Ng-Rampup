588abc77709dc8a4c5c66d6c9f10d30ad0fd2c8f
Liran An <Liran.an@sap.com>
2020-10-19 15:58:09 -0400

    add dashboard; add routerlink; add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'users', component: UsersComponent }];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
